5c0ef9d99e28b2ea9e65133691a75674e0653e25
marc <mcadoo300@gmail.com>
2024-01-08 17:20:43 -0500

    day 10 part 1

--- AOC_23/aoc_10_pt2.py
+++ AOC_23/aoc_10_pt2.py
@@ -1,74 +1,83 @@
-S="data"
-P="input_10.txt"
-with open(P,"r") as f:
-	R=f.read()
-G=R.split("\n")
-H=len(G)
-W=len(G[0])
+import copy
+import math
+input_file = open('input_10.txt','r')
+lines = input_file.readlines()
+for line in lines:
+    line = line.strip()
 
-O = [[0]*W for _ in range(H)] # part 2
+# default top down, left to right
+pipe_connections = [["|",[1,0]],["-",[0,1]],["L",[1,1]],["J",[1,-1]],["F",[1,-1]],["7",[1,1]]]
+start = None
+for row in range(len(lines)):
+    for i in range(len(lines[row])):
+        if lines[row][i] == "S":
+            print(row)
+            start = [row,i]
+            print(start)
+            break
+    if start is not None:
+        break
 
-ax = -1
-ay = -1
-for i in range(H):
-	for j in range(W):
-		if "S" in G[i]:
-			ax=i
-			ay=G[i].find("S")
-print(ax,ay)
+def Get_Possible_Adj(pipe):
+    if pipe == "|":
+        return [[-1,0],[1,0]]
+    elif pipe == "-":
+        return[[0,-1],[0,1]]
+    elif pipe == "L":
+        return[[-1,0],[0,1]]
+    elif pipe == "J":
+        return [[-1,0],[0,-1]]
+    elif pipe == "F":
+        return [[0,1],[1,0]]
+    elif pipe == "7":
+        return [[0,-1],[1,0]]
+    else:
+        return [[0],[0]]
 
-# rightward downward leftward upward
-dirs = [(0,1),(1,0),(0,-1),(-1,0)]
-happy = ["-7J", "|LJ", "-FL", "|F7"]
-Sdirs = []
-for i in range(4):
-	pos = dirs[i]
-	bx = ax+pos[0]
-	by = ay+pos[1]
-	if bx>=0 and bx<=H and by>=0 and by<=W and G[bx][by] in happy[i]:
-		Sdirs.append(i)
-print(Sdirs)
-Svalid = 3 in Sdirs # part 2
+    """elif pipe == "S":
+        [[1,0],[0,1],[-1,0],[0,-1]]"""
+    
 
-# rightward downward leftward upward
-transform = {
-	(0,"-"): 0,
-	(0,"7"): 1,
-	(0,"J"): 3,
-	(2,"-"): 2,
-	(2,"F"): 1,
-	(2,"L"): 3,
-	(1,"|"): 1,
-	(1,"L"): 0,
-	(1,"J"): 2,
-	(3,"|"): 3,
-	(3,"F"): 0,
-	(3,"7"): 2,
-}
+def Is_Valid_Connection(direction,pipe2):
+    if pipe2 == "S":
+        return True
+    op_dir = [d*-1 for d in direction]
+    if op_dir in Get_Possible_Adj(pipe2):
+        return True
+    return False
 
-curdir = Sdirs[0]
-cx = ax + dirs[curdir][0]
-cy = ay + dirs[curdir][1]
-ln = 1
-O[ax][ay] = 1 # Part 2
-while (cx,cy)!=(ax,ay):
-	O[cx][cy] = 1 # Part 2
-	ln += 1
-	curdir = transform[(curdir,G[cx][cy])]
-	cx = cx + dirs[curdir][0]
-	cy = cy + dirs[curdir][1]
-print(ln)
-print(ln//2)
 
-# End Part 1
-# Begin Part 2
 
-ct = 0
-for i in range(H):
-	inn = False
-	for j in range(W):
-		if O[i][j]:
-			if G[i][j] in "|JL" or (G[i][j]=="S" and Svalid): inn = not inn
-		else:
-			ct += inn
-print(ct)
+not_found = True
+ans = 1
+pipe_line = [start,"S"]
+for dir in [[1,0],[0,1],[-1,0],[0,-1]]:
+    row = pipe_line[0][0] + dir[0]
+    col = pipe_line[0][1] + dir[1]
+    if 0 <= row < len(lines) and 0<= col < len(lines[0])-1:
+        if lines[row][col] != ".":
+            if Is_Valid_Connection(dir,lines[row][col]):
+                pipe_line[1] = lines[row][col]
+                pipe_line[0] = [row,col]
+    if pipe_line[0] != start:
+        break
+prev_pipes = [start, pipe_line[0]]
+while not_found:
+    if pipe_line[1] == "S":
+        not_found = False
+    else:
+        for move in Get_Possible_Adj(pipe_line[1]):
+            row = pipe_line[0][0] + move[0]
+            col = pipe_line[0][1] + move[1]
+            if 0 <= row < len(lines) and 0<= col < len(lines[0])-1:
+                if Is_Valid_Connection(move,lines[row][col]) and [row,col] not in prev_pipes:
+                    pipe_line[1] = lines[row][col]
+                    pipe_line[0] = [row,col]
+                    prev_pipes.append(pipe_line[0])
+                    break
+    ans += 1
+    if ans == 3:
+        prev_pipes = prev_pipes[1:]
+    if pipe_line[1] == "S":
+        not_found = False
+print(math.ceil(ans/2))
