4013b2a612a78fbb5b06658ffc6041ad6cb4bb35
marc <mcadoo300@gmail.com>
2024-01-09 20:34:25 -0500

    old day 9

--- AOC_23/aoc_09_pt2.py
+++ AOC_23/aoc_09_pt2.py
@@ -11,11 +11,11 @@ for i in range(len(lines)):
         next_set = [(new_numbers[i+1] - new_numbers[i] )for i in range(len(new_numbers)-1)]
         set_numbers.append(next_set)
         new_numbers = next_set
-    
-    for j in range(len(set_numbers)-1,0,-1):
-        lower_set = set_numbers[j][0]
-        set_numbers[j-1].insert(0,set_numbers[j-1][0]-lower_set)
-        if j == 1:
-            ans += set_numbers[j-1][0]
+    print(set_numbers) 
+    for j in range(len(set_numbers)-2,-1,-1):
+        lower_set = set_numbers[j+1][-1]
+        set_numbers[j].append(set_numbers[j][-1]+lower_set)
+        if j == 0:
+            ans += set_numbers[j][-1]
     print(ans)
 
