86128d052c2292b567ede0fd569f10af10b2a735
marc <mcadoo300@gmail.com>
2024-01-11 09:06:57 -0500

    day 11 y 23 clean up/seperate

--- AOC_23/aoc_11_pt1.py
+++ AOC_23/aoc_11_pt1.py
@@ -17,21 +17,15 @@ for i in range(len(lines[0])):
             galaxies.append([j,i])
     if add_to_list:
         dub_cols.append(i)
-print(len(dub_cols))
-print(len(dub_rows))
 
 def Get_min_distance(p1,p2):
     distance = abs(p1[0] - p2[0])+ abs(p1[1] - p2[1])
-    """if abs(p1[0] - p2[0]) < abs(p1[1] - p2[1]):
-        distance += (abs(p1[1] - p2[1]) - abs(p1[0] - p2[0]))
-    else:
-        distance += (abs(p1[0] - p2[0]) - abs(p1[1] - p2[1]))"""
     for dub in dub_rows:
         if min(p1[0],p2[0]) < dub < max(p1[0],p2[0]):
-            distance+=999999
+            distance+=1
     for dub in dub_cols:
         if min(p1[1],p2[1]) < dub < max(p1[1],p2[1]):
-            distance+=999999
+            distance+=1
     return distance
 
 pairs = []
@@ -39,7 +33,6 @@ distances = []
 for i in range(len(galaxies)):
     for j in range(len(galaxies)):
         if j!=i and [j,i] not in pairs:
-            #print(Get_min_distance(galaxies[i],galaxies[j]))
             pairs.append([i,j])
             distances.append(Get_min_distance(galaxies[i],galaxies[j]))
 print(sum(distances))
