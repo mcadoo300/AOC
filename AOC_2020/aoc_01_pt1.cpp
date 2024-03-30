1fbb1f1210015702186d48f2b0c7734abd38ed34
Marc McAdoo <marcmcadoo123@gmail.com>
2024-03-30 11:28:41 -0400

    more practice

--- AOC_2020/aoc_01_pt1.cpp
+++ AOC_2020/aoc_01_pt1.cpp
@@ -1,40 +1,36 @@
-#include <iostream>
+#include <algorithm> // NOLINT
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-int main(){
+int main() {
   std::ifstream inf{"input_01.txt"};
-
-  if (!inf){
+  if (!inf) {
     std::cerr << "Whoops! File could not be read.\n";
     return 1;
   }
   std::vector<int> nums{};
 
-  
   std::string strInput{};
-  while ( inf >> strInput){
-    nums.insert(nums.end(),
-                stoi(strInput));
+  while (inf >> strInput) {
+    nums.insert(nums.end(), stoi(strInput));
     // std::cout << stoi(strInput) << '\n';
   }
 
-  
-  std::sort (nums.begin(),nums.end());
+  std::sort(nums.begin(), nums.end());
   // for (int i =0; i < nums.size();i++)
   //   std::cout << nums[i] << '\n';
-  int lower=0,upper=nums.size()-1,sum=0;
-  while(sum != 2020){
-    sum = nums[lower]+nums[upper];
-    if (sum < 2020){
+  int lower = 0, upper = nums.size() - 1, sum = 0;
+  while (sum != 2020) {
+    sum = nums[lower] + nums[upper];
+    if (sum < 2020) {
       lower++;
-    } else if ( sum > 2020){
+    } else if (sum > 2020) {
       upper--;
     }
   }
-  std::cout << nums[lower]*nums[upper] << '\n';
+  std::cout << nums[lower] * nums[upper] << '\n';
 
   return 0;
 }
--- AOC_2020/aoc_04_pt1.py
+++ AOC_2020/aoc_04_pt1.py
@@ -2,26 +2,27 @@ input_file = open("input_04.txt", "r")
 
 lines = input_file.readlines()
 
-fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 total_valid = 0
 is_valid = False
+seen_fld = []
+
 for row in range(len(lines)):
     line = lines[row].strip()
     if line == "":
         if is_valid:
             total_valid += 1
         is_valid = False
-    else:
         seen_fld = []
-        line = line.strip(" ")
+    else:
+        line = line.split(" ")
         for seg in line:
             seg = seg.split(":")
-            if seg[0] not in seen_fld:
+            if seg[0] != 'cid' and seg[0] not in seen_fld:
                 seen_fld.append(seg[0])
-        if len(fields)-1 == len(seen_fld):
-            if "cid" not in seen_fld:
-                is_valid = True
-        elif len(fields) == len(seen_fld):
+        if len(fields) == len(seen_fld):
             is_valid = True
+if is_valid:
+    total_valid+=1
 print(total_valid)
